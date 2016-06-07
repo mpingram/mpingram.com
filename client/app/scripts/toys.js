64852e68db83023202a5f99d68c54ba59fc19d53
Michael Ingram <mpingram92@gmail.com>
2016-06-07 17:22:36 -0500

    Tweaks to css

--- client/app/scripts/toys.js
+++ client/app/scripts/toys.js
@@ -2,12 +2,11 @@ $(document).on('click touchend',
 	'#about, #music, #websites, #projects',
 	function(event){
 		var id = event.currentTarget.id;
+		// identify id of popin box
 		var box = $('#box-'+id); 
 		if(box.hasClass('active')){
 			box.removeClass('active');
 		} else {
-			// removeClass from all four boxes
-			// add class to box.
 			box.addClass('active').siblings().removeClass('active');
 		}
 	}
