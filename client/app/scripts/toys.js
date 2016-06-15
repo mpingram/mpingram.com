4ef0df9ce77411c8342991dc17ef80076e854476
Michael Ingram <mpingram92@gmail.com>
2016-06-15 15:09:49 -0500

    Tweak images, add scaffolding for popin boxes

--- client/app/scripts/toys.js
+++ client/app/scripts/toys.js
@@ -1,12 +1,31 @@
+// resets boxes and icons to neutral
+function resetPage(boxId){
+
+}
+
+// moves boxes and icons according
+// to which corner of the screen they're in
+function moveBoxes(boxId){
+
+}
+
+
+// click event handler for all four icons
 $(document).on('click touchend', '#about, #music, #websites, #projects',
 	function(event){
 		var id = event.currentTarget.id;
-		// identify id of popin box
-		var box = $('#box-'+id); 
+		console.log(event.currentTarget.id);
+		var box = $('#'+id+'-box'); 
+		// if box is already open, close it
 		if(box.hasClass('active')){
 			box.removeClass('active');
+		// else close any open boxes and open the selected box
 		} else {
 			box.addClass('active').siblings().removeClass('active');
 		}
 	}
 );
+
+
+
+
