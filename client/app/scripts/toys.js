e414212ba13ba5e91a7d33fded598a3374234adf
Michael Ingram <mpingram92@gmail.com>
2016-06-16 14:24:02 -0500

    Icons moving, but at what cost?
    Really weird thing with inconsisent left/right shifting.
    Going to delete styling and copy in the left popin box
    until I figure out what it is. Also, mobile layout
    still huge issue, it's beginning to do the thing I want
    (line up in a row on the sides) but it's going to take
    a lot of finnagling. Is that how you spell that?

--- client/app/scripts/toys.js
+++ client/app/scripts/toys.js
@@ -3,7 +3,7 @@ $(document).on('click touchend', '#about, #music, #websites, #projects',
 	function(event){
 		var id = event.currentTarget.id;
 		var box = $('#'+id+'-box'); 
-		var center = $('#center-container');
+		var toybox = $('#toybox');
 
 		// stores where center should move
 		var location;
@@ -24,11 +24,11 @@ $(document).on('click touchend', '#about, #music, #websites, #projects',
 
 		// if box is already open, close it and move the center back to neutral
 		if(box.hasClass('active')){
-			center.removeClass(location);
+			toybox.removeClass(location);
 			box.removeClass('active');
 		// else move the center and slide in the box
 		} else {
-			center.addClass(location);
+			toybox.removeClass().addClass(location);
 			box.addClass('active').siblings().removeClass('active');
 		}
 	}
