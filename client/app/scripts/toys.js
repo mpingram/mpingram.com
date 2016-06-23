e95f9a4ad20e5e7c5c22d1d362e356b82f4d0fda
Michael Ingram <mpingram92@gmail.com>
2016-06-23 15:46:28 -0500

    Commit before changing popin box style
    toys.js in an even greater state of disarray. It's a disarray
    kind of day I guess

--- client/app/scripts/toys.js
+++ client/app/scripts/toys.js
@@ -7,21 +7,17 @@
 
 	$(document).ready(function init(){
 
+		// variable delcaration
+		// --------------------
 		var toybox = $('#toybox');
 		var centerpiece = $('#centerpiece');
+		// stores active popin box
+		var activeBox;
+		// stores active location
 		var location;
 
-		function reset(){
-			centerpiece.removeClass('collapsed');
-			toybox.removeClass();
-			$('.active').removeClass('active');
-		}
-
-		function moveCenterpiece(direction){
-		}
-
-
-		// hammer.js touch event handler
+		// hammerjs initialization
+		// ----------------------
 		var body = document.getElementById('body-wrapper');
 		var hammer = new Hammer(body);
 		var hammerManager = new Hammer.Manager(body,{
@@ -29,46 +25,73 @@
 				[Hammer.Swipe, {directon: Hammer.DIRECTION_ALL}]
 			]
 		});
+		var swipes = [];
+		swipes[2] = 'left';
+		swipes[4] = 'right';
+		swipes[8] = 'up';
+		swipes[16] = 'down';
+
+
+
+		// helper functions
+		// --------------------
+		function setLocation(){
+			switch(activeBox.selector){
+				// TODO:
+				// this is a dumb data structure
+				case '#about-box':
+					location='right';
+					break;
+				case '#music-box':
+					location='up';
+					break;
+				case '#websites-box':
+					location='down';
+					break;
+				case '#projects-box':
+					location='left';
+					break;
+			}
+		}
+		// move centerpiece and popin boxes back to neutral
+		function reset(){
+			location = undefined;
+			centerpiece.removeClass('collapsed');
+			toybox.removeClass();
+			$('.active').removeClass('active');
+		}
+		// move centerpiece and popin boxes according to active box
+		function move(){
+			centerpiece.addClass('collapsed');
+			toybox.removeClass().addClass(location);
+			activeBox.addClass('active').siblings().removeClass('active');
+		}
+
+
+
+		// hammer.js touch event handler
 		hammerManager.on('swipe', function(ev){
+			if (!location){
+				location = swipes[ev.direction];
+				move();
+			}
 			// TODO: remove this ridiculous testing hack
 			reset();
 		});
 
 		// click event handler for all four icons
 		$(document).on('click', '#about, #music, #websites, #projects',
-			function iconHandler(event){
+			function iconClickHandler(event){
 				var id = event.currentTarget.id;
-				var box = $('#'+id+'-box'); 
-
-				// where center should move
-				switch(id){
-					case 'about':
-						location='right';
-						break;
-					case 'music':
-						location='top';
-						break;
-					case 'websites':
-						location='bottom';
-						break;
-					case 'projects':
-						location='left';
-						break;
-				}
+				activeBox = $('#'+id+'-box'); 
 
 				// if box is already open, close it and move the center back to neutral
-				if(box.hasClass('active')){
+				if(activeBox.hasClass('active')){
 					reset();
-					/*
-					centerpiece.removeClass('collapsed');
-					toybox.removeClass();
-					box.removeClass('active');
-					*/
 				// else move the center and slide in the box
 				} else {
-					centerpiece.addClass('collapsed');
-					toybox.removeClass().addClass(location);
-					box.addClass('active').siblings().removeClass('active');
+					setLocation(activeBox);
+					move();
 				}
 			}
 		);
