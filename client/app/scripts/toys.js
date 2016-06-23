698d78c9071111df2192377e6b4f36d9389342ee
Michael Ingram <mpingram92@gmail.com>
2016-06-23 13:55:20 -0500

    Simple gesture support, simple about scaffolding
    toys.js (need to change that name) is in the process of
    being moved around, and I also don't like the about window
    lol. It feels too cramped in mobile and too spaced out
    on desktop, worst of both worlds. Also I like the green
    but that's it.

--- client/app/scripts/toys.js
+++ client/app/scripts/toys.js
@@ -1,50 +1,79 @@
-// click event handler for all four icons
-$(document).on('click', '#about, #music, #websites, #projects',
-	function(event){
-		var id = event.currentTarget.id;
-		var box = $('#'+id+'-box'); 
+// IDEA: wrap in IIFE for local scope, then
+// store location in js so that can act without
+// having to interact with the dom?
+// TODO: add reset and move function and call them from click
+// and swipe event handlers
+(function($, window, document){
+
+	$(document).ready(function init(){
+
 		var toybox = $('#toybox');
 		var centerpiece = $('#centerpiece');
-
-		// where center should move
 		var location;
-		switch(id){
-			case 'about':
-				location='right';
-				break;
-			case 'music':
-				location='top';
-				break;
-			case 'websites':
-				location='bottom';
-				break;
-			case 'projects':
-				location='left';
-				break;
-		}
 
-		// if box is already open, close it and move the center back to neutral
-		if(box.hasClass('active')){
+		function reset(){
 			centerpiece.removeClass('collapsed');
 			toybox.removeClass();
-			box.removeClass('active');
-		// else move the center and slide in the box
-		} else {
-			centerpiece.addClass('collapsed');
-			toybox.removeClass().addClass(location);
-			box.addClass('active').siblings().removeClass('active');
+			$('.active').removeClass('active');
 		}
-	}
-);
 
-// hammer.js touch event handler
-var body = document.getElementById('body-wrapper');
-var hammer = new Hammer(body);
-var hammerManager = new Hammer.Manager(body,{
-	recognizers: [
-		[Hammer.Swipe, {directon: Hammer.DIRECTION_ALL}]
-	]
-});
-hammerManager.on('swipe', function(ev){
-	console.log(ev);
-});
+		function moveCenterpiece(direction){
+		}
+
+
+		// hammer.js touch event handler
+		var body = document.getElementById('body-wrapper');
+		var hammer = new Hammer(body);
+		var hammerManager = new Hammer.Manager(body,{
+			recognizers: [
+				[Hammer.Swipe, {directon: Hammer.DIRECTION_ALL}]
+			]
+		});
+		hammerManager.on('swipe', function(ev){
+			// TODO: remove this ridiculous testing hack
+			reset();
+		});
+
+		// click event handler for all four icons
+		$(document).on('click', '#about, #music, #websites, #projects',
+			function iconHandler(event){
+				var id = event.currentTarget.id;
+				var box = $('#'+id+'-box'); 
+
+				// where center should move
+				switch(id){
+					case 'about':
+						location='right';
+						break;
+					case 'music':
+						location='top';
+						break;
+					case 'websites':
+						location='bottom';
+						break;
+					case 'projects':
+						location='left';
+						break;
+				}
+
+				// if box is already open, close it and move the center back to neutral
+				if(box.hasClass('active')){
+					reset();
+					/*
+					centerpiece.removeClass('collapsed');
+					toybox.removeClass();
+					box.removeClass('active');
+					*/
+				// else move the center and slide in the box
+				} else {
+					centerpiece.addClass('collapsed');
+					toybox.removeClass().addClass(location);
+					box.addClass('active').siblings().removeClass('active');
+				}
+			}
+		);
+
+	// end document.ready
+	});
+// end IIFE
+})(window.jQuery, window, document);
