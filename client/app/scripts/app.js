ed54b99fe0806bcf6a69f66d8a5d47927ed5421e
Michael Ingram <mpingram92@gmail.com>
2016-07-11 11:53:08 -0500

    Achieve understanding of misplaced svgs issue
    It defo has to do with the way the page loads content - which explains
    the differences cross-browser. I would have hoped that doc.ready would
    avoid that kind of thing, but oh well. I tested the thing by redrawing the
    svgs using a manual call to the drawSVG function, which I also stuck into
    a global namespace variable. Please remember to remove that from your code
    if you don't need to call the drawSVG function from the dom anywhere (which
    you probably wont!)

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -2,6 +2,10 @@
 
 	$(document).ready(function init(){
 
+	// 0: DEFINE GLOBAL NAMESPACE
+	// TODO: necessary?
+	window.appNamespace = {};
+
 
 	// I. EVENT HANDLERS
 	// ====================
@@ -122,111 +126,125 @@
 
 	// II. ABOUT-SKILLS-LIST CONNECTING LINES
 	// =======================
-		// draws SVG lines connecting element in
-		// the skill list with elements in the tech list
-
-		// store skills and technology jQuery elements
-		var skillItems = {};
-		var techItems = {};
-		var svgElement = $('#svg-container');
-
-		// match skills to technologies via IDs
-		// TODO: brittle, is there a smarter way?
-		var skillTechTable = {
-			'webApps': [
-				'javascript',
-				'angularJS',
-				'jQuery',
-				'html',
-				'css'
-			],
-			'restApi': [
-				'javascript',
-				'nodeJS',
-				'expressJS'
-			],
-			'server': [
-				'javascript',
-				'nodeJS',
-				'expressJS'
-			],
-			'databaseManagement': [
-				'mongoDB',
-				'mongoose',
-				'mySQL'
-			],
-			'webDeployment': [
-				'git',
-				'AWS'
-			],
-			'webDesign': [
-				'html',
-				'css',
-				'illustrator', 
-				'jQuery'
-			] 
-		};
-
-		// TODO: define svg create function - make sure the markup is good - give it a class
-		var createSVG = function(positionObj){
-
-			var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-			svg.setAttributeNS(null, 'class','about-skills-svg');
-
-			var svgLine = document.createElementNS('http://www.w3.org/2000/svg','line');
-			svgLine.setAttributeNS(null, 'class','about-skills-line');
-			svgLine.setAttributeNS(null, 'stroke-width','1px');
-			svgLine.setAttributeNS(null, 'stroke','black');
-
-			svgLine.setAttributeNS(null, 'x1', positionObj.x1);
-			svgLine.setAttributeNS(null, 'y1', positionObj.y1);
-			svgLine.setAttributeNS(null, 'x2', positionObj.x2);
-			svgLine.setAttributeNS(null, 'y2', positionObj.y2);
-			svg.appendChild(svgLine);
-			return svg;
-		};
 
 
-		// stick tech jQuery element into container object, we'll need it soon.
-		$('#about-tech-column').find('.about-tech-item').each(function(index, elem){
-			techItems[elem.id] = $(this);
-		});
-		// iterate over skill items and draw the svgs we need.
-		$('#about-skills-column').find('.about-skills-item').each(function(){
-			var skillItem = $(this);
 
-			// stick skill jQuery element into container object, we'll need it to
-			// efficiently update the svg lines on window.resize events.
-			skillItems[skillItem[0].id] = skillItem;
-			var skillPosition =  skillItem.position();
+		function drawSVG(){
+			// TODO: remove previous svgs on redraw.
+			// another reason why having just one svg would be easier.
 
+			// draws SVG lines connecting element in
+			// the skill list with elements in the tech list
 
-			// get the array of tech item IDs associated with this skill
-			var technologies = skillTechTable[skillItem[0].id];
+			// store skills and technology jQuery elements
+			var skillItems = {};
+			var techItems = {};
+			var svgElement = $('#svg-container');
 
-			var positionObj = {
-				// todo: this sucks, i want the mid right
-				// can use clientHeight/clientWidth jquery obj props to fix
-				'x1':skillPosition.left + skillItem[0].clientWidth,
-				'y1':skillPosition.top + skillItem[0].clientHeight/2
+			// match skills to technologies via IDs
+			// TODO: brittle, is there a smarter way?
+			var skillTechTable = {
+				'webApps': [
+					'javascript',
+					'angularJS',
+					'jQuery',
+					'html',
+					'css'
+				],
+				'restApi': [
+					'javascript',
+					'nodeJS',
+					'expressJS'
+				],
+				'server': [
+					'javascript',
+					'nodeJS',
+					'expressJS'
+				],
+				'databaseManagement': [
+					'mongoDB',
+					'mongoose',
+					'mySQL'
+				],
+				'webDeployment': [
+					'git',
+					'AWS'
+				],
+				'webDesign': [
+					'html',
+					'css',
+					'illustrator', 
+					'jQuery'
+				] 
 			};
-			for (var i=0;i<technologies.length;i++){
 
-				// get position of this tech element
-				var techItem = techItems[technologies[i]];
-				var techPosition = techItem.position();
-				positionObj.x2 = techPosition.left;
-				positionObj.y2 = techPosition.top + techItem[0].clientHeight/2;
+			// TODO: define svg create function - make sure the markup is good - give it a class
+			var createSVG = function(positionObj){
 
-				// draw svg line connecting skill element to tech element
-				var line = createSVG(positionObj);
-				// stick the svg line into the dom
-				svgElement.append(line);
+				var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+				svg.setAttributeNS(null, 'class','about-skills-svg');
 
-			}
+				var svgLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+				svgLine.setAttributeNS(null, 'class','about-skills-line');
+				svgLine.setAttributeNS(null, 'stroke-width','1px');
+				svgLine.setAttributeNS(null, 'stroke','black');
+
+				svgLine.setAttributeNS(null, 'x1', positionObj.x1);
+				svgLine.setAttributeNS(null, 'y1', positionObj.y1);
+				svgLine.setAttributeNS(null, 'x2', positionObj.x2);
+				svgLine.setAttributeNS(null, 'y2', positionObj.y2);
+				svg.appendChild(svgLine);
+				return svg;
+			};
 
 
-		});
+			// stick tech jQuery element into container object, we'll need it soon.
+			$('#about-tech-column').find('.about-tech-item').each(function(index, elem){
+				techItems[elem.id] = $(this);
+			});
+			// iterate over skill items and draw the svgs we need.
+			$('#about-skills-column').find('.about-skills-item').each(function(){
+				var skillItem = $(this);
+
+				// stick skill jQuery element into container object, we'll need it to
+				// efficiently update the svg lines on window.resize events.
+				skillItems[skillItem[0].id] = skillItem;
+				var skillPosition =  skillItem.position();
+
+
+				// get the array of tech item IDs associated with this skill
+				var technologies = skillTechTable[skillItem[0].id];
+
+				var positionObj = {
+					// todo: this sucks, i want the mid right
+					// can use clientHeight/clientWidth jquery obj props to fix
+					'x1':skillPosition.left + skillItem[0].clientWidth,
+					'y1':skillPosition.top + skillItem[0].clientHeight/2
+				};
+				for (var i=0;i<technologies.length;i++){
+
+					// get position of this tech element
+					var techItem = techItems[technologies[i]];
+					var techPosition = techItem.position();
+					positionObj.x2 = techPosition.left;
+					positionObj.y2 = techPosition.top + techItem[0].clientHeight/2;
+
+					// draw svg line connecting skill element to tech element
+					var line = createSVG(positionObj);
+					// stick the svg line into the dom
+					svgElement.append(line);
+
+				}
+			});
+
+		// end drawSvg()
+		}
+
+		// call drawSVG()
+		//drawSVG();
+
+		// DEBUG: export drawSVG()
+		window.appNamespace.drawSVG = drawSVG;
 
 		// TODO: handle window.resize() events with updateSVG function
 
