2d8008f46fd325f03f5ae1270f974cd1b8fbe62d
Michael Ingram <mpingram92@gmail.com>
2016-12-02 18:01:24 -0600

    Overlay commentary proof of concept
    - no commentary yet tho

--- client/public/eventmanager/parent/scripts/overlay.js
+++ client/public/eventmanager/parent/scripts/overlay.js
@@ -1,15 +1,117 @@
 $(document).ready( function(){
 
+
+
+	// commentary
+	// ===============================
 	
-	// commentary helper functions
-	// =================================
+	// commentary configuration
+	// -------------------------------------
+	var commentaryArray = [
+		{
+			'element': 'em-event-list',
+			'message': 'Test',
+			'link': 'http://google.com',
+			'offset': {
+				'top': -20,
+				'left': 10
+			}
+		},
+	];
+	
+
+	// helper jquery method
+	$.fn.exists = function(){
+		return this.length !== 0;
+	};
+
+	// commentary creation functions
+	// ----------------------------------
+	var $overlayCommentaryContainer = $('#overlay-commentary-container');
+
+	function createCommentaryBubble( options ){
+
+		var message = options.message;
+		var link = options.link;
+
+		var $elem = $(options.element);
+		if ( $elem.exists() === false ){
+			throw new Error( 'Element not found: ' + options.element );
+		}
+		var position = $elem.offset();
+		var adjustment = options.offset;
+
+		var location = {
+			left: ( position.left + adjustment.left ) + 'px',
+			top: 	( position.top + adjustment.top ) + 'px'
+		};
+
+		// commentary bubble creation
+		var $commentaryBubbleTemplate = $('<div/>', {
+			'class': 'commentary-bubble',
+
+		}).css({ 
+			'top': location.top, 
+			'left': location.left 
+		});
+
+		var $commentaryMessage = $('<div/>', {
+			'class': 'commentary-message'
+		}).text( message );
+
+		var $commentaryLink = $('<div/>', {
+			'class': 'commentary-link'
+		}).text( link );
+
+		var $commentaryBubble = $commentaryBubbleTemplate.append( $commentaryMessage, $commentaryLink );
+
+		$overlayCommentaryContainer.append( $commentaryBubble );
+
+	}
+
+
+	// commentary display functions
+	// ------------------------------------
 	function showCommentary(){
-
+		$('.commentary-bubble').show();
 	}
-	
+
 	function hideCommentary(){
+		$('.commentary-bubble').hide();
+	}
+
+
+	
+	
+	// commentary initialization functions
+	// ------------------------------------
+	function writeCommentary( commentaryArray ){
+
+		for( var i=0; i < commentaryArray.length; i++){
+			try {
+				createCommentaryBubble( commentaryArray[i] );
+			} catch( error ){
+				console.warn( error.message );
+			}
+		}
 
 	}
+
+
+	
+
+
+	/////////////////////
+	// commentary init //
+	/////////////////////
+	// ----------------------------------- 
+	
+	// TODO: call after angular initialized somehow
+	// or try catch try again?
+	setTimeout( function(){
+		writeCommentary( commentaryArray );
+	}, 1000);
+
 
 
 
@@ -30,6 +132,10 @@ $(document).ready( function(){
 
 	});
 
+	var $returnParentButton = $('#return-parent-button');
+	$returnParentButton.on( 'click', function(){
+		// ohhh boy
+	});
 
 
 
